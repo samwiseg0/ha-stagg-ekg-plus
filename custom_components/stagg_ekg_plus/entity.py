dec30207bc9b9f558669365545732d89392853f1
Samwiseg0 <2241731+samwiseg0@users.noreply.github.com>
2026-06-17 22:29:22 -0400

    fix: import DeviceInfo from device_registry

--- custom_components/stagg_ekg_plus/entity.py
+++ custom_components/stagg_ekg_plus/entity.py
@@ -18,8 +18,7 @@
 
 from __future__ import annotations
 
-from homeassistant.helpers.device_info import DeviceInfo
-from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH
+from homeassistant.helpers.device_registry import CONNECTION_BLUETOOTH, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN, MANUFACTURER, MODEL
